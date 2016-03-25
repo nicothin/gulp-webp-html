9b80a0dcf49ba0027a59e21760850fe64163b2d2
Kirill <seokirillstarsnab@gmail.com>
2016-03-25 16:24:23 +0300

    start

---
+++ README.md
@@ -0,0 +1,27 @@
+# gulp-webp-css
+
+## Example
+```html
+// Input
+<img src="/img/tmp/catalogImage.jpg">
+
+// Output
+<picture>
+    <source srcset="/img/tmp/catalogImage.webp" type="image/webp">
+    <img src="/img/tmp/catalogImage.jpg">
+</picture>
+```
+## Install
+```bash
+npm i --save-dev gulp-webp-html
+```
+## Usage
+```javascript
+var webpHTML = require('gulp-webp-html');
+
+gulp.task('html',function(){
+    gulp.src('./assets/**/*.html')
+        .pipe(webpHTML())
+        .pipe(gulp.dest('./public/'))
+});
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+const gutil = require('gulp-util');
+const PluginError = gutil.PluginError;
+const through = require('through2');
+
+const pluginName = 'gulp-webp-css';
+
+module.exports = function(extensions){
+    var extensions = extensions || ['.jpg','.png','.jpeg','JPG'];
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new PluginError(pluginName, 'Streaming not supported'));
+            return;
+        }
+
+        try {
+            var pictureRender = function (url, imgTag) {
+                return '<picture><source srcset="' + url + '" type="image/webp">' + imgTag + '</picture>';
+            }
+
+            var inPicture = false;
+
+
+            const data = file.contents.toString()
+                    .split('\n')
+                    .map(function(line){
+                        // Вне <picture/>?
+                        if (line.indexOf('<picture') + 1) inPicture = true;
+                        if (line.indexOf('</picture') + 1) inPicture = false;
+
+                        // Проверяем есть ли <img/>
+                        if((line.indexOf('<img') + 1) && !inPicture){
+                            // Новый урл с .webp
+                            var Re = /<img([^>]*)src=\"(\S+)\">/ig;
+                            var regexpArray = Re.exec(line);
+                            var imgTag = regexpArray[0];
+                            var newUrl = regexpArray[2];
+                            // Если в урле есть .webp, пропускаем
+                            if (newUrl.indexOf('.webp') + 1) return line;
+                            // Заменяем все расширения на .webp
+                            for(k in extensions){
+                                newUrl = newUrl.replace(extensions[k],'.webp')
+                            }
+                            // Компилим <picture/>
+                            var newHTML = pictureRender(newUrl,imgTag);
+                            return line.replace(imgTag,newHTML);
+                        }
+                        return line;
+                    })
+                    .join('');
+            file.contents = new Buffer(data);
+            this.push(file);
+        } catch (err) {
+            this.emit('error', new PluginError(pluginName, err));
+        }
+
+        cb();
+    });
+};
